709231a24e64040f8910f59c9732e7ec41fdc4cd
Joe Van Dyk <joe@fixieconsulting.com>
2011-01-03 17:48:48 -0800

    Added ruby_enterprise and bash-vim cookbooks

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -1,9 +1,39 @@
+
 %w(ssh
    sudo
    users
    apt
+   bash-vim
    ppa
-  ).each do |recipe_name|
+   ruby_enterprise
+).each do |recipe_name|
     include_recipe recipe_name
 end
 
+
+%w(vim
+   man-db
+   libxml2-dev
+   libxslt1-dev
+   build-essential
+   git-core
+   libmagick++-dev
+   libcurl4-openssl-dev
+   command-not-found
+   htop
+   sysstat
+).each do |name|
+  package name
+end
+
+gems = {
+  "bundler"   => "1.0.7",
+  "rake"      => "0.8.7",
+}
+
+gems.each do |gem, v|
+  ree_gem gem do
+    action :install
+    version v
+  end
+end
